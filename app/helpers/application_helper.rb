250b8042f1636d87244a7bb8a557537a6f95c10c
Greg Tarsa <gltarsa@gmail.com>
2016-12-05 19:03:28 -0500

    Moves controller logic out of view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,17 @@
 module ApplicationHelper
+
+  def latest_postings
+    Posting.order(created_at: :desc).take(latest_count)
+  end
+
   # The last 10 or fewer are the "latest"
-  def latest_posts
+  def latest_count
     [Posting.count, 10].min
+  end
+
+  private
+
+  def after_sign_in_path_for(resource)
+    root_path
   end
 end
